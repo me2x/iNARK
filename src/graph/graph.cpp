782c18aa66b376aa5df26a6bbca8f925202c83e7
me2x <vitali.ema@hotmail.it>
2015-12-02 16:30:25 +0100

    common header creation with #ifdef still work in progress

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "iNARKConfig.h"
-
 #include "graph.h"
 
 #include <boost/filesystem.hpp>
---
+++ src/graph/internal_graph.cpp
@@ -0,0 +1,3 @@
+#include "internal_graph.h"
+
+
