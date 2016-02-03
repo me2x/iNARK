8e2eb4a4965a5d1a470f337124ab9e279ceb6ee8
me2x <vitali.ema@hotmail.it>
2016-02-03 11:01:06 +0100

    add of common.cpp: this file will contain the node classes with the explosion methods implementations

---
+++ src/graph/graph_common.cpp
@@ -0,0 +1,9 @@
+#include "graph_common.hpp"
+
+
+void First_Level_Vertex::explode_component_timing(Timing_Graph& graph, std::map<std::string, std::map< int, std::string> >& components_map ) const {
+    timing_vertex_t vt = boost::add_vertex(graph);
+    graph[vt].name = this->name;
+    graph[vt].layer = this->layer;
+    
+}
